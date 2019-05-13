bb9bf62e3613e1f4d0f0e83e8f2e937093d19a06
Qin, Michael <Michael.Qin@tdameritrade.com>
2019-05-13 13:14:30 -0400

    add copyright

--- .mvn/wrapper/MavenWrapperDownloader.java
+++ .mvn/wrapper/MavenWrapperDownloader.java
@@ -1,21 +1,20 @@
-/*
-Licensed to the Apache Software Foundation (ASF) under one
-or more contributor license agreements.  See the NOTICE file
-distributed with this work for additional information
-regarding copyright ownership.  The ASF licenses this file
-to you under the Apache License, Version 2.0 (the
-"License"); you may not use this file except in compliance
-with the License.  You may obtain a copy of the License at
-
-  https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing,
-software distributed under the License is distributed on an
-"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-KIND, either express or implied.  See the License for the
-specific language governing permissions and limitations
-under the License.
-*/
+/*******************************************************************************
+ * Copyright (c) 2019 Michael Qin
+ *
+ * The freedom to run the program as you wish, for any purpose (freedom 0).
+ *
+ * The freedom to study how the program works, and change it so it does your computing 
+ * as you wish (freedom 1). Access to the source code is a precondition for this.
+ *
+ * The freedom to redistribute copies so you can help your neighbor (freedom 2).
+ *
+ * The freedom to distribute copies of your modified versions to others (freedom 3). 
+ * By doing this you can give the whole community a chance to benefit from your changes. 
+ * Access to the source code is a precondition for this.
+ *
+ * Contributors:
+ *     Michael Qin - initial API and implementation
+ *******************************************************************************/
 
 import java.io.File;
 import java.io.FileInputStream;
